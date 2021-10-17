082ed0a16324f83286fd4bc4d71b7c696d0b24ff
gonnavis <gonnavis@163.com>
2021-10-17 22:06:30 +0800

    pok: doubleJump && clean log.

--- role.js
+++ role.js
@@ -140,7 +140,7 @@ class Role {
               hit: { target: 'hit' },
               dash: { target: 'jumpDash' },
             },
-            tags: ['canMove'],
+            // tags: ['canMove'],
           },
           hit: {
             entry: ['playHit'],
@@ -181,7 +181,7 @@ class Role {
             // let scalingFactor = 50
             // resultantImpulse.op_mul(scalingFactor)
             s.body.setLinearVelocity(resultantImpulse)
-            console.log('setLinearVelocity')
+            // console.log('setLinearVelocity')
           },
           entryDash() {
             s.fadeToAction('dash', 0.2)
@@ -251,7 +251,7 @@ class Role {
             // }, 200)
           },
           jump() {
-            console.warn('jump')
+            // console.warn('jump')
 
             //cannon
             // s.body.velocity.y = 20
@@ -261,7 +261,7 @@ class Role {
             // let scalingFactor = 50
             // resultantImpulse.op_mul(scalingFactor)
             s.body.setLinearVelocity(resultantImpulse)
-            console.log('setLinearVelocity')
+            // console.log('setLinearVelocity')
             s.banCollideDetect = true
           },
           playJump() {
@@ -378,7 +378,7 @@ class Role {
 
     s.body.onCollide = (event) => {
       if (s.banCollideDetect) return
-      console.warn('collide')
+      // console.warn('collide')
       // console.log('collide', event.body.id, event.target.id)
       if (event.body === window.ground.body) {
         // todo: refactor: window.ground
@@ -425,7 +425,7 @@ class Role {
         let scalingFactor = 50
         resultantImpulse.op_mul(scalingFactor)
         s.body.setLinearVelocity(resultantImpulse)
-        console.log('setLinearVelocity')
+        // console.log('setLinearVelocity')
       }
 
       // s.gltf.scene.position.set(s.body.position.x, s.body.position.y - body_size, s.body.position.z)

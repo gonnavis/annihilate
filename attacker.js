d9c2dc872ca774440ba6e11838add05285e4f7e1
gonnavis <gonnavis@163.com>
2021-11-14 04:47:31 +0800

    add enemys' friendly fire damage.

--- attacker.js
+++ attacker.js
@@ -2,10 +2,10 @@
 class Attacker {
   constructor(scene = scene, updates = updates /*arr*/, owner /*vec3*/, target /*vec3*/) {
     let s = this
-    let movement /*vec3*/ = vec3().subVectors(target, owner).normalize().multiplyScalar(0.5)
+    let movement /*vec3*/ = vec3().subVectors(target, owner.body.position).normalize().multiplyScalar(0.5)
 
     let body_size = 2
-    s.is_hit = false
+    // s.is_hit = false
     s.body = new CANNON.Body({
       mass: 0,
       type: CANNON.Body.KINEMATIC,
@@ -14,7 +14,7 @@ class Attacker {
     let shape = new CANNON.Cylinder(body_size, body_size, 0.3, 8)
     s.body.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
     s.body.addShape(shape)
-    s.body.position.copy(owner)
+    s.body.position.copy(owner.body.position)
     world.addBody(s.body)
 
     s.body.addEventListener('collide', (e) => {
@@ -25,11 +25,17 @@ class Attacker {
 
       // console.log(a===e)
 
-      if (!s.is_hit && e.body === role.body) {
-        s.is_hit = true
+      // if (!s.is_hit && e.body === role.body) { // used stateMachine, should not need is_hit.
+      if (e.body === role.body) {
+        // s.is_hit = true
         // console.log('asdf')
         role.hit()
       }
+      window.enemys.forEach((enemy) => {
+        if (e.body === enemy.body && e.body !== owner.body) {
+          enemy.hit()
+        }
+      })
     })
 
     function update() {

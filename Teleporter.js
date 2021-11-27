6b5a09a3f2489a89c48cd2bdf4c805b93fec218e
gonnavis <gonnavis@163.com>
2021-11-28 04:27:46 +0800

    auto-rotate teleporter.

--- Teleporter.js
+++ Teleporter.js
@@ -3,7 +3,10 @@ import { g } from './global.js'
 import * as THREE from './lib/three.js/build/three.module.js'
 class Teleporter {
   constructor(width = 1, height = 1, depth = 1) {
+    window.updates.push(this)
+
     this.dest = new THREE.Vector3()
+    // this.rand = Math.random()
 
     this.mesh = new THREE.Mesh(new THREE.BoxBufferGeometry(width, height, depth), new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true }))
     this.mesh.castShadow = true
@@ -23,6 +26,11 @@ class Teleporter {
       }
     })
     world.addBody(this.body)
+  }
+
+  update(dt, time) {
+    this.mesh.rotation.x = time * 0.003
+    this.mesh.rotation.y = time * 0.003
   }
 }
 

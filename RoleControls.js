c2c04c6e044420f5d20ed454bdc54da5e887dd18
gonnavis <gonnavis@163.com>
2021-11-28 14:56:12 +0800

    doing: add: "pop" skill, use key J+K+L. && rename: oKey -> holdKey. && add: tickKey. && move key check from keyDown event to update() loop.

--- RoleControls.js
+++ RoleControls.js
@@ -8,8 +8,9 @@ class RoleControls {
 
     updates.push(this)
 
-    this.oKey = {}
-    this.actkey = ''
+    this.holdKey = {}
+    this.tickKey = {}
+    // this.actkey = ''
 
     window.addEventListener('keydown', (e) => {
       // console.log('repeat', e.repeat, e.code)
@@ -17,14 +18,49 @@ class RoleControls {
       // if (e.repeat) return
       // console.log(e.key, e.code, e.keyCode)
 
-      if (this.oKey[e.code]) return // Prevent: keyD -> keyJ long press -> keyD up, cause double attack bug.
+      if (this.holdKey[e.code]) return // Prevent: keyD -> keyJ long press -> keyD up, cause double attack bug.
       // e.prepeat & if (e.code === this.actkey) return, both not work.
 
-      this.oKey[e.code] = true
+      this.holdKey[e.code] = true
+      this.tickKey[e.code] = true
 
       // if (!this.gltf) return
       // if (e.code === this.actkey) return
+      // this.actkey = e.code
+
+      // console.log(Object.fromEntries(Object.entries(this.holdKey).filter((n) => n[1])))
+      // console.log(
+      //   Object.entries(this.holdKey)
+      //     .filter((n) => n[1])
+      //     .map((n) => n[0])
+      // )
+    })
+    window.addEventListener('keyup', (e) => {
+      // console.log(e)
+      this.holdKey[e.code] = false
+
       switch (e.code) {
+        case 'KeyJ':
+        case 'Numpad4':
+          this.role.service.send('keyJUp')
+          break
+        case 'KeyU':
+        case 'Numpad7':
+          this.role.service.send('keyUUp')
+          break
+      }
+      // this.actkey = ''
+    })
+  }
+
+  update(dt) {
+    // console.log('update')
+    // if (this.role.service.state.matches('loading')) return
+
+    if (this.tickKey.KeyJ && this.tickKey.KeyK && this.tickKey.KeyL) {
+      console.log('pop')
+    } else {
+      switch (Object.keys(this.tickKey)[0]) {
         case 'KeyJ':
         case 'Numpad4':
           this.role.service.send('attack')
@@ -43,42 +79,13 @@ class RoleControls {
           this.role.service.send('bash')
           break
       }
-      this.actkey = e.code
-
-      // console.log(Object.fromEntries(Object.entries(this.oKey).filter((n) => n[1])))
-      console.log(
-        Object.entries(this.oKey)
-          .filter((n) => n[1])
-          .map((n) => n[0])
-      )
-    })
-    window.addEventListener('keyup', (e) => {
-      // console.log(e)
-      this.oKey[e.code] = false
-
-      switch (e.code) {
-        case 'KeyJ':
-        case 'Numpad4':
-          this.role.service.send('keyJUp')
-          break
-        case 'KeyU':
-        case 'Numpad7':
-          this.role.service.send('keyUUp')
-          break
-      }
-      this.actkey = ''
-    })
-  }
-
-  update(dt) {
-    console.log('update')
-    // if (this.role.service.state.matches('loading')) return
+    }
 
     this.role.direction.set(0, 0)
-    if (this.oKey.KeyW || this.oKey.ArrowUp) this.role.direction.add(vec2(0, -1))
-    if (this.oKey.KeyS || this.oKey.ArrowDown) this.role.direction.add(vec2(0, 1))
-    if (this.oKey.KeyA || this.oKey.ArrowLeft) this.role.direction.add(vec2(-1, 0))
-    if (this.oKey.KeyD || this.oKey.ArrowRight) this.role.direction.add(vec2(1, 0))
+    if (this.holdKey.KeyW || this.holdKey.ArrowUp) this.role.direction.add(vec2(0, -1))
+    if (this.holdKey.KeyS || this.holdKey.ArrowDown) this.role.direction.add(vec2(0, 1))
+    if (this.holdKey.KeyA || this.holdKey.ArrowLeft) this.role.direction.add(vec2(-1, 0))
+    if (this.holdKey.KeyD || this.holdKey.ArrowRight) this.role.direction.add(vec2(1, 0))
     this.role.direction.normalize().multiplyScalar(this.role.speed)
     // console.log(this.role.direction)
     let directionLengthSq = this.role.direction.lengthSq()
@@ -101,13 +108,16 @@ class RoleControls {
       this.role.body.position.z += this.role.direction.y
     }
 
-    // if (this.oKey.KeyJ) {
+    // if (this.holdKey.KeyJ) {
     //   // code here run after animation finished event, in one tick.
     //   this.role.service.send('keyJDown')
     //   // console.log('whirlwind')
     // } else {
     //   this.role.service.send('keyJUp')
     // }
+
+    // restore
+    this.tickKey = {}
   }
   setRole(role) {
     this.role = role

5e455e1c7c86f3264d93a0fd53ad573891a219cd
gonnavis <gonnavis@163.com>
2022-10-22 14:57:38 +0800

    Add: xml-parser.js. && Del: source mapping.

--- src/Maria.js
+++ src/Maria.js
@@ -25,10 +25,10 @@ import {
 } from '../lib/modular-behavior-tree/index.js'
 // import * as BT from '../lib/modular-behavior-tree/index.js'
 
-console.log(SUCCESS);
-console.log(FAILURE);
-console.log(RUNNING);
-console.log(MemSequence);
+// console.log(SUCCESS);
+// console.log(FAILURE);
+// console.log(RUNNING);
+// console.log(MemSequence);
 
 class Maria {
   constructor(x, y, z) {
@@ -86,6 +86,81 @@ class Maria {
     // scene.add(this.shadow)
     
     // --- behavior tree
+
+    let testCount1 = 0;
+    let testCount2 = 0;
+    class F0 extends Action {
+        constructor({properties = { text: null }} = {}) {
+            super({properties})
+        }
+        start() {
+            console.log('*** 000 start')
+        }
+        run(blackboard) {
+            console.log('000 SUCCESS')
+            return SUCCESS
+        }
+        end() {
+            console.log('*** 000 end')
+        }
+    }
+    class F1 extends Action {
+        constructor({properties = { text: null }} = {}) {
+            super({properties})
+        }
+        start() {
+            console.log('*** 111 start')
+        }
+        run(blackboard) {
+            testCount1++;
+            if (testCount1 <= 3) {
+              console.log('111 RUNNING')
+              return RUNNING
+            } else {
+              console.log('111 SUCCESS')
+              return SUCCESS
+            }
+        }
+        end() {
+            console.log('*** 111 end')
+        }
+    }
+    class F2 extends Action {
+        constructor({properties = { text: null }} = {}) {
+            super({properties})
+        }
+        start() {
+            console.log('*** 222 start')
+        }
+        run(blackboard) {
+            testCount2++;
+            if (testCount2 <= 3) {
+              console.log('222 RUNNING')
+              return RUNNING
+            } else {
+              console.log('222 SUCCESS')
+              return SUCCESS
+            }
+        }
+        end() {
+            console.log('*** 222 end')
+        }
+    }
+    
+    
+    let mainTree = BehaviorTree.parseFileXML('./example/maintree.xml', {F0, F1, F2})
+    
+    var bt = new BehaviorTree({tree: mainTree, blackboard: {someVariable: 123}})
+
+    let count = 0;
+    async function step() {
+      await bt.tick()
+      console.log('-----------------------------')
+      count++;
+      if (count < 10) setTimeout(step, 1000);
+    }
+    step();
+
     // --- end: behavior tree
 
     let physicsMaterial = new CANNON.Material({
